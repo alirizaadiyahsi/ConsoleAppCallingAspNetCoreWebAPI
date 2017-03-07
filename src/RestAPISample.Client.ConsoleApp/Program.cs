e8f24b68c16c42caa66dafef1aa858fa6b4c7a76
aadiyahsi <aadiyahsi@TRWS7980>
2017-03-07 10:07:33 +0300

    Add project files.

---
+++ src/RestAPISample.Client.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using RestAPISample.Domain;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RestAPISample.Client.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://localhost:54741/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var users = GetUsers(client);
+
+            foreach (var item in users.Result)
+            {
+                Console.WriteLine(item.Id + " " + item.Name);
+            }
+
+            Console.ReadLine();
+        }
+
+        static async Task<List<User>> GetUsers(HttpClient client)
+        {
+            var users = new List<User>();
+
+            using (client)
+            {
+                HttpResponseMessage response = await client.GetAsync("api/user");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    users = await response.Content.ReadAsAsync<List<User>>();
+                }
+            }
+
+            return users;
+        }
+    }
+}
---
+++ src/RestAPISample.Web/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestAPISample.Domain;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestAPISample.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public List<User> Get()
+        {
+            return new  List<User>{
+                new User {
+                    Id = 1,
+                    Name = "user 1" },
+                new User {
+                    Id = 2,
+                    Name = "user 2" },
+                new User {
+                    Id = 3,
+                    Name = "user 3" },
+                new User {
+                    Id = 4,
+                    Name = "user 4" }
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return new User
+            {
+                Id = 5,
+                Name = "user 5"
+            };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]User user)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]User user)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
